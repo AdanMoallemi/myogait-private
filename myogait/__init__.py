8322cb192f1d836971cee8d1b1f8dfaaf07b3790
Adan Moallemi <adanmoallemi@host231-98.vpn.ubc.ca>
2026-08-21 14:31:08 -0700

    Remove stroboscopic diagram from pipeline and plotting modules

--- myogait/__init__.py
+++ myogait/__init__.py
@@ -159,7 +159,6 @@ _LAZY_EXPORT_MAP = {
     "plot_cadence_profile": ".plotting",
     "plot_rom_summary": ".plotting",
     "plot_butterfly": ".plotting",
-    "plot_stroboscopic_cycle": ".plotting",
     "animate_normative_comparison": ".plotting",
     "generate_report": ".report",
     "generate_longitudinal_report": ".report",
